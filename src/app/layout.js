35c4c7f597a85c549da701e5b6d63b96f0f77e29
Karim Badwork <karim@badwork.fr>
2025-10-27 22:11:14 +0100

    feat: ajouter une classe de fond au composant RootLayout

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,8 +19,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang='en'>
-			<body className={`${russoOne.className}`}>{children}</body>
+		<html lang='en' className='bodyBg'>
+			<body className={`${russoOne.className} bodyBg`}>{children}</body>
 		</html>
 	);
 }
