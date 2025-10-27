546a3ac27bdd6fbfc31dff9a251474da76484c44
Karim Badwork <karim@badwork.fr>
2025-10-27 19:59:20 +0100

    feat: importer la police Russo One dans le composant Home

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,8 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import FuzzyText from "@/components/FuzzyText";
 
+import { russoOne } from "./fonts";
+
 export default function Home() {
 	return (
 		<div className={styles.page}>
