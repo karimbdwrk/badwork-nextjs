42240c5297ebe7bb88781a14adaafc589d9dea10
Karim Badwork <karim@badwork.fr>
2025-10-28 00:33:19 +0100

    feat: ajouter le support de la police Geist et exporter la variable correspondante

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,6 +1,11 @@
-import { Russo_One } from "next/font/google";
+import { Geist, Russo_One } from "next/font/google";
 
 export const russoOne = Russo_One({
   subsets: ["latin"],
   weight: "400",
+});
+
+export const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
