34b9ff190b945e4e80372a38436dd74b673eb5cb
Karim Badwork <karim@badwork.fr>
2025-10-28 01:08:29 +0100

    fix: nettoyer le code en commentant les éléments inutilisés dans le composant Home

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,17 @@
 "use client";
 
 import Image from "next/image";
+
+// import {
+// 	Modal,
+// 	ModalContent,
+// 	ModalHeader,
+// 	ModalBody,
+// 	ModalFooter,
+// 	Button,
+// 	useDisclosure,
+// } from "@heroui/react";
+
 import styles from "./page.module.css";
 // import FuzzyText from "../components/FuzzyText";
 import Dither from "../components/Dither";
@@ -8,6 +19,8 @@ import Dither from "../components/Dither";
 import { russoOne } from "./fonts";
 
 export default function Home() {
+	// const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
 	return (
 		<>
 			<div
@@ -57,10 +70,12 @@ export default function Home() {
 					</div>
 					<div className={styles.ctas}>
 						<a
+							// onPress={onOpen}
 							className={styles.primary}
-							href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-							target='_blank'
-							rel='noopener noreferrer'>
+							// href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+							// target='_blank'
+							// rel='noopener noreferrer'
+						>
 							{/* <Image
 								className={styles.logo}
 								src='/vercel.svg'
@@ -70,6 +85,64 @@ export default function Home() {
 							/> */}
 							CONTACT US
 						</a>
+						{/* <Button onPress={onOpen}>Open Modal</Button>
+						<Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+							<ModalContent>
+								{(onClose) => (
+									<>
+										<ModalHeader className='flex flex-col gap-1'>
+											Modal Title
+										</ModalHeader>
+										<ModalBody>
+											<p>
+												Lorem ipsum dolor sit amet,
+												consectetur adipiscing elit.
+												Nullam pulvinar risus non risus
+												hendrerit venenatis.
+												Pellentesque sit amet hendrerit
+												risus, sed porttitor quam.
+											</p>
+											<p>
+												Lorem ipsum dolor sit amet,
+												consectetur adipiscing elit.
+												Nullam pulvinar risus non risus
+												hendrerit venenatis.
+												Pellentesque sit amet hendrerit
+												risus, sed porttitor quam.
+											</p>
+											<p>
+												Magna exercitation reprehenderit
+												magna aute tempor cupidatat
+												consequat elit dolor
+												adipisicing. Mollit dolor
+												eiusmod sunt ex incididunt
+												cillum quis. Velit duis sit
+												officia eiusmod Lorem aliqua
+												enim laboris do dolor eiusmod.
+												Et mollit incididunt nisi
+												consectetur esse laborum eiusmod
+												pariatur proident Lorem eiusmod
+												et. Culpa deserunt nostrud ad
+												veniam.
+											</p>
+										</ModalBody>
+										<ModalFooter>
+											<Button
+												color='danger'
+												variant='light'
+												onPress={onClose}>
+												Close
+											</Button>
+											<Button
+												color='primary'
+												onPress={onClose}>
+												Action
+											</Button>
+										</ModalFooter>
+									</>
+								)}
+							</ModalContent>
+						</Modal> */}
 						{/* <a
 							className={styles.primary}
 							href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
